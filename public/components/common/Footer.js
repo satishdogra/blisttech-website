eb70dc9d6bee42c9a5272a997841c03a8495dbcb
gurujaswal06 <gurujaswal06@gmail.com>
2025-01-07 18:01:08 +0530

    I have worked on header,footer,teambanner modules

--- public/components/common/Footer.js
+++ public/components/common/Footer.js
@@ -4,9 +4,10 @@ import styles from './Footer.module.css';
 export default function Footer() {
   return (
     <>
-    <footer className={styles.footer}>
-      <h1 className={styles.footerHeading}>Footer</h1>
-    </footer>
+      <footer className={styles.footer}>
+        <h1 className={styles.footerHeading}>Footer</h1>
+        <h2>I am Working on Guru-Designer branch</h2>
+      </footer>
     </>
   )
 }
